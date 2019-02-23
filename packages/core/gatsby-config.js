b795e6e19972330d02ef762a66c8f09e30ed2010
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-02-23 14:06:36 +0100

    bootstrap gatsby-theme-graphcms

---
+++ packages/core/gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = ({ endpoint }) => ({
+  plugins: [
+    // Simple config, passing URL
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "GCMS",
+        // This is field under which it's accessible
+        fieldName: "gcms",
+        // Url to query from
+        url: endpoint,
+      },
+    },
+  ]
+})
